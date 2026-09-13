6ad34afe0a55367c6e2b7f3c584f2ffa0c26ea24
loogansbluff-collab <loogansbluff@gmail.com>
2026-09-13 15:22:36 -0600

    Center Batch 1 ceiling fixtures so room lighting reads from eye level

--- src/components/interior/fixtures/CeilingDisc.tsx
+++ src/components/interior/fixtures/CeilingDisc.tsx
@@ -11,10 +11,14 @@ export default function CeilingDisc({ position, radius, emissiveColor, lightColo
   return (
     <group position={position}>
       <mesh>
-        <cylinderGeometry args={[radius, radius, 0.1, 28]} />
-        <meshStandardMaterial color="#e5e7eb" emissive={emissiveColor} emissiveIntensity={2.2} roughness={0.5} />
+        <cylinderGeometry args={[radius + 0.1, radius + 0.1, 0.18, 28]} />
+        <meshStandardMaterial color="#3f454b" roughness={0.78} />
       </mesh>
-      <pointLight position={[0, -0.2, 0]} color={lightColor} intensity={intensity} distance={distance} castShadow={false} />
+      <mesh position={[0, -0.12, 0]}>
+        <cylinderGeometry args={[radius, radius, 0.07, 28]} />
+        <meshStandardMaterial color="#f8f8f6" emissive={emissiveColor} emissiveIntensity={2.6} roughness={0.36} />
+      </mesh>
+      <pointLight position={[0, -0.32, 0]} color={lightColor} intensity={intensity} distance={distance} castShadow={false} />
     </group>
   );
 }
--- src/components/interior/fixtures/FluorescentPanel.tsx
+++ src/components/interior/fixtures/FluorescentPanel.tsx
@@ -8,13 +8,19 @@ type Props = {
 };
 
 export default function FluorescentPanel({ position, size, emissiveColor, lightColor, intensity, distance }: Props) {
+  const housingHeight = Math.max(size[1], 0.18) + 0.12;
+
   return (
     <group position={position}>
       <mesh>
-        <boxGeometry args={size} />
-        <meshStandardMaterial color="#d8dde2" emissive={emissiveColor} emissiveIntensity={2.4} roughness={0.45} />
+        <boxGeometry args={[size[0] + 0.16, housingHeight, size[2] + 0.16]} />
+        <meshStandardMaterial color="#343a40" roughness={0.75} />
       </mesh>
-      <pointLight position={[0, -0.22, 0]} color={lightColor} intensity={intensity} distance={distance} castShadow={false} />
+      <mesh position={[0, -(housingHeight / 2 + 0.045), 0]}>
+        <boxGeometry args={[size[0], 0.09, size[2]]} />
+        <meshStandardMaterial color="#f7f8fa" emissive={emissiveColor} emissiveIntensity={2.8} roughness={0.32} />
+      </mesh>
+      <pointLight position={[0, -(housingHeight / 2 + 0.28), 0]} color={lightColor} intensity={intensity} distance={distance} castShadow={false} />
     </group>
   );
 }
